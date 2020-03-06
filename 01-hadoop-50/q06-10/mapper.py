2f8973c3dea1b7176b039e7c2e05ec245743970d
Milady Aguilera Cardona <50975760+amaguilerac@users.noreply.github.com>
2020-03-06 16:52:30 -0500

    Update mapper.py

--- 01-hadoop-50/q06-10/mapper.py
+++ 01-hadoop-50/q06-10/mapper.py
@@ -3,3 +3,11 @@ import sys
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 
+next(sys.stdin) ##para quitar encabezado
+if __name__ == "__main__":
+
+       for line in sys.stdin:
+         c1,c2,c3 = line.split("   ")
+         c3=c3.strip()
+                   
+         sys.stdout.write("{},{}\n".format(c1,c3))

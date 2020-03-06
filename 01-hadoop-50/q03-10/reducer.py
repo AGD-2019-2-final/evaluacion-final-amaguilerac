d3506dc39545a5d0755034243d70345351288026
Milady Aguilera Cardona <50975760+amaguilerac@users.noreply.github.com>
2020-03-06 15:59:47 -0500

    Update reducer.py

--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -2,3 +2,14 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+   
+
+    for line in sys.stdin:
+
+        val, key = line.split(",")
+        key = key.strip()
+        sys.stdout.write("{},{}\n".format(key,val))

6d46ea13913291268ae312aa9d82af9edf455b6c
Milady Aguilera Cardona <50975760+amaguilerac@users.noreply.github.com>
2020-03-06 17:04:46 -0500

    Update reducer.py

--- 01-hadoop-50/q04-10/reducer.py
+++ 01-hadoop-50/q04-10/reducer.py
@@ -3,12 +3,15 @@ import sys
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 
-if __name__ == '__main__':
+if _name_ == '_main_':
 
-      
 
+    ##
+    ## cada linea de texto recibida es una
+    ## entrada clave \tabulador valor
+    ##
     for line in sys.stdin:
-
-        fecha,letra,valor = line.split(",")
-        valor=valor.strip()
-        sys.stdout.write("{},{},{}\n".format(letra,fecha,valor))
+      
+      fecha, letra, val = line.split(",")
+      val=val.strip()
+      sys.stdout.write("{},{},{}\n".format(letra, fecha, val))

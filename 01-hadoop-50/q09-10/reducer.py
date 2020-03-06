78911f99689bff5994f07be151de51644e2ddb9c
Milady Aguilera Cardona <50975760+amaguilerac@users.noreply.github.com>
2020-03-06 17:28:28 -0500

    Update reducer.py

--- 01-hadoop-50/q09-10/reducer.py
+++ 01-hadoop-50/q09-10/reducer.py
@@ -2,3 +2,13 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+contador = 0
+
+for line in sys.stdin:
+  contador=contador+1
+  val,fecha,letra = line.split(",")
+  val=int(val)
+  letra=letra.strip()
+  
+  if contador <= 6:
+    sys.stdout.write("{}   {}   {}\t\n".format(letra,fecha,val))

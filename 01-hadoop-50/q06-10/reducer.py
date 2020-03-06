9a33c6be46d40661669270d096ce5952a77d8f17
Milady Aguilera Cardona <50975760+amaguilerac@users.noreply.github.com>
2020-03-06 16:53:21 -0500

    Update reducer.py

--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -2,3 +2,31 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+curkey = None
+total = 0
+curmax=0
+curmin=100000000
+
+
+for line in sys.stdin:
+
+    key, val = line.split(",")
+    val=float(val)
+
+    if key == curkey:
+        if val > curmax:
+          curmax=val
+        if val<curmin:
+          curmin=val
+        total += val
+    else:
+        
+        if curkey is not None:
+            
+            sys.stdout.write("{}\t{}\t{}\n".format(curkey, curmax,curmin))
+
+        curkey = key
+        curmin=val
+        curmax= val
+
+sys.stdout.write("{}\t{}\t{}\n".format(curkey, curmax,curmin))

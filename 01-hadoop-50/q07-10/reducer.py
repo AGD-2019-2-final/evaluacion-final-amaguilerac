94aa2c980c88eca3147de8effa8348e5fd228252
Milady Aguilera Cardona <50975760+amaguilerac@users.noreply.github.com>
2020-03-06 16:55:38 -0500

    Update reducer.py

--- 01-hadoop-50/q07-10/reducer.py
+++ 01-hadoop-50/q07-10/reducer.py
@@ -2,3 +2,8 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+for line in sys.stdin:
+
+    c1,c2,c3 = line.split(",")
+    c3 = c3.strip()
+    sys.stdout.write("{}   {}   {}\t\n".format(c1,c3,c2))

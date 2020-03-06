813fd7673e248f766d79e2e0944a253c4b315184
Milady Aguilera Cardona <50975760+amaguilerac@users.noreply.github.com>
2020-03-06 15:56:07 -0500

    Update mapper.py

--- 01-hadoop-50/q02-10/mapper.py
+++ 01-hadoop-50/q02-10/mapper.py
@@ -2,3 +2,23 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+next(sys.stdin)
+if __name__ == "__main__":
+
+    ##
+    ## itera sobre cada linea de codigo recibida
+    ## a traves del flujo de entrada
+    ##
+    for line in sys.stdin:
+
+        ##
+        ## genera las tuplas palabra \tabulador 1
+        ## ya que es un conteo de palabras
+        ##
+        i=0
+        for word in line.split(','):
+            i=i+1
+            if i==4:
+               sys.stdout.write("{}\t".format(word))
+            if i==5:
+               sys.stdout.write("{}\n".format(word))
